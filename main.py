2881b12ec2abc98235caa56b45e173863dff83df
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2023-05-12 18:08:42 +0530

    feat: Added pretty line

--- main.py
+++ main.py
@@ -2,14 +2,21 @@ from youtube_comments import fetchPostComments
 from text_analyzer import analyzeText
 from spam_detecter import isTextSpam
 
+def printPretty(lines):
+    print("---------------------------------------------------------------")
+    for line in lines:
+        print(line)
+        print()
+    print("---------------------------------------------------------------\n")
+
+
 def main():
     youtube_video_url = 'https://www.youtube.com/watch?v=ZtzAwBzKE7c&ab_channel=SuhaanBhandary'    
     comments = fetchPostComments(youtube_video_url, 10, False)
 
     for comment in comments:
         if(isTextSpam(comment["text"])):
-            print(comment["text"])
-            print(analyzeText(comment["text"]))
+            printPretty([comment["text"], analyzeText(comment["text"])])
 
 if __name__ == "__main__":
     main()

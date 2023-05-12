b729f9f9a11617f19316321ec9e752deed70ab97
atharvabhavsar01 <atharvabhavsar01@gmail.com>
2023-05-12 16:24:34 +0530

    updated spam detection

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from youtube_comments import fetchPostComments
 from text_analyzer import analyzeText
+from spam_detecter import isTextSpam
 
 def main():
-    youtube_video_url = 'https://www.youtube.com/watch?v=5eKSQT5mV-c'
+    youtube_video_url = 'https://www.youtube.com/watch?v=ZtzAwBzKE7c&ab_channel=SuhaanBhandary'
+
+    dummy = ['Huh, anyway check out this you[tube] channel: kobyoshi02']
+    print(isTextSpam(dummy[0]))
+    return
 
     print("Running the main function")
-    comments = fetchPostComments(youtube_video_url, 5, True)
+    comments = fetchPostComments(youtube_video_url, 10, False)
 
     for comment in comments:
+        print(1)
         print(comment["text"])
-        print(analyzeText(comment["text"]))
-        print()
+        if(isTextSpam(comment["text"][0])):
+            print(analyzeText(comment["text"]))
 
 if __name__ == "__main__":
     main()
